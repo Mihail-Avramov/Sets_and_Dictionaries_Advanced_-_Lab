a73c9cc88aeb103e67b0c8f0d558bdb2bc68d659
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-19 11:55:41 +0200

    T04

---
+++ T04. Product Shop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+SortedDictionary<string,Dictionary<String, double>> foodShopsInformation = new SortedDictionary<string, Dictionary<string, double>>();
+
+string input;
+
+while ((input = Console.ReadLine()) != "Revision")
+{
+    string[] informationArguments = input.Split(", ").ToArray();
+
+    string shopName = informationArguments[0];
+    string foodName = informationArguments[1];
+    double price = double.Parse(informationArguments[2]);
+
+    if (!foodShopsInformation.ContainsKey(shopName))
+    {
+        foodShopsInformation[shopName] = new Dictionary<string, double>();
+    }
+
+    foodShopsInformation[shopName][foodName] = price;
+}
+
+foreach (var shop in foodShopsInformation)
+{
+    Console.WriteLine($"{shop.Key}->");
+    foreach (var food in shop.Value)
+    {
+        Console.WriteLine($"Product: {food.Key}, Price: {food.Value}");
+    }
+}
