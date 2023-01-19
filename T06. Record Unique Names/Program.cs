2a46b79ecb5bbc3dc72996de53ce31395da24b7a
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-19 12:21:42 +0200

    T06

---
+++ T06. Record Unique Names/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+int n = int.Parse(Console.ReadLine());
+
+HashSet<string> namesHashSet = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    namesHashSet.Add(Console.ReadLine());
+}
+
+foreach (var name in namesHashSet)
+{
+    Console.WriteLine(name);
+}
