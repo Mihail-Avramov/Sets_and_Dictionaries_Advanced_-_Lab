77e36ef20cce63c530b4f40024e0b318b7640b83
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-19 11:38:51 +0200

    T03

---
+++ T03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+int[] numbers = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Console.WriteLine(string.Join(" ", numbers.OrderByDescending(x => x).Take(3)));
