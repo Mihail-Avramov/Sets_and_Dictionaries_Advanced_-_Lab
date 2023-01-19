5234ddc1fc84b42ffe34147943d094ec33013b35
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-19 11:33:05 +0200

    Add project files.

---
+++ T01. Count Same Values in Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+Dictionary<double, int> doubleCountValues = new Dictionary<double, int>();
+
+double[] numbers = Console.ReadLine()
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .ToArray();
+
+foreach (var number in numbers)
+{
+    if (!doubleCountValues.ContainsKey(number))
+    {
+        doubleCountValues.Add(number,0);
+    }
+
+    doubleCountValues[number]++;
+}
+
+foreach (var kvp in doubleCountValues)
+{
+    Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+}
---
+++ T02. Average Student Grades/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> studentsGrades = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] inputArguments = Console.ReadLine().Split(' ');
+    string name = inputArguments[0];
+    decimal grade = decimal.Parse(inputArguments[1]);
+
+    if (!studentsGrades.ContainsKey(name))
+    {
+        studentsGrades[name] = new List<decimal>();
+    }
+
+    studentsGrades[name].Add(grade);
+}
+
+foreach (var studentGrades in studentsGrades)
+{
+    Console.WriteLine($"{studentGrades.Key} -> {string.Join(" ", studentGrades.Value.Select(x => $"{x:f2}"))} (avg: {studentGrades.Value.Average():f2})");
+}
